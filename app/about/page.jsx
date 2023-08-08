4caa83a31e877920a0a2a2fce073688fae694677
tomal <hasnathshahriartamal@gmail.com>
2023-08-08 18:00:27 +0600

    the structure is ok

---
+++ app/about/page.jsx
@@ -0,0 +1,13 @@
+import Button from '../components/Button';
+import styles from './about.module.css';
+
+const AboutPage = () => {
+  return (
+    <div className={styles.main}>
+      <h1>Hi there</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default AboutPage;
